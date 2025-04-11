00406cb66f21bf8006510dcc62c4682ee3fa831a
Edicleo Oliveira <119067190+edicleoline@users.noreply.github.com>
2025-04-10 21:51:16 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -3,3 +3,16 @@ SELECT c.customer_id, c.first_name, c.last_name
   FROM customers c
   LEFT JOIN orders o ON c.customer_id = o.customer_id
 WHERE o.order_id IS NULL;
+
+--Listar os Produtos que não tenham sido comprados--
+SELECT p.product_id, p.product_name
+  FROM products p
+  LEFT JOIN order_items oi ON p.product_id = oi.product_id
+WHERE oi.product_id IS NULL;
+
+--Listar os Produtos sem Estoque--
+SELECT p.product_id, p.product_name
+  FROM products p
+  LEFT JOIN stocks s ON p.product_id = s.product_id
+GROUP BY p.product_id, p.product_name
+  HAVING COALESCE(SUM(s.quantity), 0) = 0;
